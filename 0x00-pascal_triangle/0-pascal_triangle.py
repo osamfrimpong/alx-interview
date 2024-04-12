cbb11065df9f0dfdcd96dd0503a336dcee123b1e
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2024-04-12 03:32:47 +0000

    Added Solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
+print(pascal_triangle(0))
